bad15ff44e77c2a241d8ddb391b1876274072c1c
mgKhantt <khantphonenaing156@gmail.com>
2024-11-25 18:21:36 +0630

    Initial Commit

---
+++ Apple_Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple_Frameworks
+//
+//  Created by Khant Phone Naing  on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
