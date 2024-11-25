2ae45856a75fd1039e9df2cce75c1b33d4d32458
mgKhantt <khantphonenaing156@gmail.com>
2024-11-25 19:25:33 +0630

    Nav and scroll view added

--- Apple_Frameworks/Framework.swift
+++ Apple_Frameworks/Framework.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 
-struct Framework {
+struct Framework: Hashable, Identifiable {
+    
+    let id = UUID()
     let name: String
     let imageName: String
     let urlString: String
     let description: String
+    
 }
 
 struct MockData {
--- Apple_Frameworks/FrameworkGridView.swift
+++ Apple_Frameworks/FrameworkGridView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("Apple Frameworks")
+        }
     }
 }
 
@@ -18,19 +34,19 @@ struct FrameworkGridView: View {
 }
 
 struct FrameworkTitleView : View {
-    let name: String
-    let imageName: String
+   
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
-        }
+        }.padding()
     }
 }
